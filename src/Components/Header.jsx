9233278496ecaa1b22ea1fc0abf448f7793d4293
ayatbd <ayatbd.571@gmail.com>
2023-04-10 03:01:58 +0600

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav>
+            <h4>JobLit</h4>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <button>Star Applying</button>
+        </nav>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home";
+import ErrorPage from "./Components/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children:[
+      {path: "/",
+      element: <Home></Home>
+    }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
